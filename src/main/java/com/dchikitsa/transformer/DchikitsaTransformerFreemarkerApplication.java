fdffe87ce3c9a65b8dbf884d4626d6a02087c66c
vedprakash1510 <87705515+vedprakash1510@users.noreply.github.com>
2022-10-18 17:52:53 +0530

    first commit

---
+++ src/main/java/com/dchikitsa/transformer/DchikitsaTransformerFreemarkerApplication.java
@@ -0,0 +1,13 @@
+package com.dchikitsa.transformer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DchikitsaTransformerFreemarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DchikitsaTransformerFreemarkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dchikitsa/transformer/DchikitsaTransformerFreemarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dchikitsa.transformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DchikitsaTransformerFreemarkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
